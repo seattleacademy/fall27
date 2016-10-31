2998bd66e9cd8d0083f4beb8ae8765bb763e8e98
saas student <sylviabergerud@seattleacademy.org>
2016-10-31 19:38:16 +0000

    add leds

---
+++ ledstest.js
@@ -0,0 +1,14 @@
+var nodeimu = require('nodeimu');
+var IMU = new nodeimu.IMU();
+
+var PythonShell = require('python-shell');
+
+PythonShell.run('helloworld.py', function (err, results) {
+  if (err) throw err;
+});
+
+IMU.getValue(function(err, data) {
+    if (err) throw err;
+    console.log(data);
+});
+
